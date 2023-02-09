2c383778a43097add7e009849668bbcdea8c7b86
zoparga <zoltan@pappz.hu>
2023-02-09 07:12:57 +0100

    body -> longtext

--- src/database/migrations/2018_05_11_115355_use_longtext_for_attachments.php
+++ src/database/migrations/2018_05_11_115355_use_longtext_for_attachments.php
@@ -3,28 +3,31 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class UseLongtextForAttachments extends Migration {
+class UseLongtextForAttachments extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('email_log', function (Blueprint $table) {
+            $table->longText('attachments')->nullable()->change();
+            $table->longText('body')->change();
+        });
+    }
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up() {
-		Schema::table('email_log', function (Blueprint $table) {
-			$table->longText('attachments')->nullable()->change();
-		});
-	}
-
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down() {
-		Schema::table('email_log', function (Blueprint $table) {
-			$table->text('attachments')->nullable()->change();
-		});
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('email_log', function (Blueprint $table) {
+            $table->text('attachments')->nullable()->change();
+            $table->text('body')->change();
+        });
+    }
 }

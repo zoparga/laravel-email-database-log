c27ac2cb6cc4881eba235d65b053c08ab8af2b2a
zoparga <zoltan@pappz.hu>
2023-02-09 07:03:37 +0100

    init

---
+++ src/zoparga/LaravelEmailDatabaseLog/LaravelEmailDatabaseLogEventServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace zoparga\LaravelEmailDatabaseLog;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Mail\Events\MessageSending;
+
+class LaravelEmailDatabaseLogEventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        MessageSending::class => [
+            EmailLogger::class,
+        ],
+    ];
+}
